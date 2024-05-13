84baecaa520540be85151c9fd3718b7da65757c4
ysrhee6199 <83895074+ysrhee6199@users.noreply.github.com>
2024-05-13 12:41:34 +0900

    Update LV.launch.py

--- scale_truck_control_ros2/launch/LV.launch.py
+++ scale_truck_control_ros2/launch/LV.launch.py
@@ -5,7 +5,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable, Shutdown
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch.substitutions import PathJoinSubstitution
